39ce24ba576d4dc01381ad88991665f3d63c1a0b
devMarshall <onyendo52@gmail.com>
2019-07-04 17:03:08 +0100

    chore: code linting

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-
+import { AppRegistry } from 'react-native';
 import {
   setCustomText,
   setCustomTextInput,
 } from 'react-native-global-props';
-
-import {name as appName} from './app.json';
+import App from './App';
+import { name as appName } from './app.json';
 
 const customTextProps = {
   style: {
