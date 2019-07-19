f65b20b0cabbbc68e06b6370df9cf83bfbea0cc3
devMarshall <onyendo52@gmail.com>
2019-07-19 08:46:55 +0100

    chore: integrate react-native config

--- android/app/src/main/java/com/muvit/MainApplication.java
+++ android/app/src/main/java/com/muvit/MainApplication.java
@@ -3,6 +3,7 @@ package com.muvit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.rnfs.RNFSPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new RNFSPackage(),
             new RNGestureHandlerPackage()
       );
