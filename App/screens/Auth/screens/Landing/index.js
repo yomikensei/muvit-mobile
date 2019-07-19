79b50563e36d1963285330e58dbbc922aa9889d4
devMarshall <onyendo52@gmail.com>
2019-07-19 09:38:21 +0100

    chore: revamp landing

--- App/screens/Auth/screens/Landing/index.js
+++ App/screens/Auth/screens/Landing/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable global-require */
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import { Button, Icon } from 'native-base';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import colors from '../../../../constants/colors.json';
 
 const styles = StyleSheet.create({
@@ -22,9 +22,24 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: colors.primary,
     color: '#ffffff',
-    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
     marginVertical: 10,
-    elevation: 0,
+    top: 20,
+    borderRadius: 7,
+    elevation: 20,
+    width: 180,
+    height: 50,
+  },
+  text_button: {
+    color: '#ffffff',
+    paddingHorizontal: 10,
+    textAlignVertical: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  icon_button: {
+    textAlignVertical: 'center',
   },
 });
 
@@ -41,14 +56,13 @@ class Landing extends Component {
           resizeMode="contain"
         />
         <Text style={styles.text}>Quick deliveries anytime, anywhere</Text>
-        <View>
-          <Button iconRight rounded style={styles.button} onPress={() => navigate('Login')}>
-            <Text style={{
-              color: '#ffffff',
-            }}>Get started</Text>
-            <Icon name="md-arrow-round-forward" />
-          </Button>
-        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => navigate('Login')}>
+          <Text style={styles.text_button}>
+            Get started
+          </Text>
+          <Icon style={styles.icon_button} name="angle-double-right" size={20} color="white" />
+        </TouchableOpacity>
       </View>
     );
   }
--- android/app/src/main/java/com/muvit/MainApplication.java
+++ android/app/src/main/java/com/muvit/MainApplication.java
@@ -3,6 +3,7 @@ package com.muvit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.rnfs.RNFSPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new ReactNativeConfigPackage(),
             new RNFSPackage(),
             new RNGestureHandlerPackage()
